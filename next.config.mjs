48b1428092bee0c8e42045763dc424c58922d163
Nurul Insani Dewi <nurulinsanid@gmail.com>
2025-06-05 20:28:10 +0700

    add assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+    remotePatterns: [new URL('https://seminyak.hotelindigo.com/**')],
+  },
+};
 
 export default nextConfig;

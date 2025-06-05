c215dff3d0050a82ea3dffb6f1b59d293feb7ba4
Nurul Insani Dewi <nurulinsanid@gmail.com>
2025-06-05 23:47:20 +0700

    fix Hydration Error

--- component/landing/instagram-widget.jsx
+++ component/landing/instagram-widget.jsx
@@ -1,21 +1,40 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const InstagramWidget = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://cdn.lightwidget.com/widgets/lightwidget.js";
+    script.async = true;
+    document.body.appendChild(script);
+    
+    setIsMounted(true);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <section className="container container-y-padding">
       <div className="text-center mb-[30px] space-y-4">
         <h2 className="uppercase text-[2.2rem]">HIT US UP ON THE GRAM</h2>
-        <Link href="/instagram.com" className="link-btn" target="_blank">GO SOCIAL</Link>
+        <Link href="https://instagram.com" className="link-btn" target="_blank">GO SOCIAL</Link>
       </div>
       <div>
-        <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
-        <iframe
-          title="Instagram feed from our official account"
-          src="https://cdn.lightwidget.com/widgets/1d28e1aebc5c5c53945abe4385a9e4a2.html"
-          allowtransparency="true"
-          className="lightwidget-widget w-full"
-       />
+        {isMounted && (
+          <iframe
+            title="Instagram feed from our official account"
+            src="https://cdn.lightwidget.com/widgets/1d28e1aebc5c5c53945abe4385a9e4a2.html"
+            allowtransparency="true"
+            className="lightwidget-widget w-full"
+            style={{ border: 0, overflow: "hidden" }}
+          />
+        )}
       </div>
     </section>
   );
